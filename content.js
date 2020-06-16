9fd1d9c1ae14b63368b4bf56957b47a2e8407616
Chris Loy <cloy202@gmail.com>
2020-06-16 20:13:04 +0200

    Added drop area to every newly appearing article in the endless stream with mutationobserver

--- content.js
+++ content.js
@@ -2,27 +2,66 @@
 //////////////////////////// 9GAG /////////////////////////////
 
 if(window.location.href.startsWith("https://9gag.com/")){
-    console.log('////////////////////////////////////////');
-    let ids = [];
-    $("article").each(function() {
-        ids.push(this.id);
-    });
-    ids.forEach(id => {
-        $(`#${id} > div.post-container`).prepend(`
-            <div class='drop-area-container'>
-                <div class="drop-area">
-                    <button id="drop_${id}">drop</button>
-                </div>
-            </div>
-        `)
-        $(`drop_${id}`).bind('click', () => {
-            console.log(id);
+
+    // const dropPic = (id) => { 
+    //     const article = $(`#${id}`);
+    //     let postData = {
+    //         id: id.substring(8)
+    //         title: article.find("h1").innerHTML,
+    //         imgUrl: 
+    //     }
+    //     //TODO: send data to backend script here
+    // }
+
+    getIdsForStreamContainer = (streamContainerId) => {
+        const articles = $(`#${streamContainerId}`).find('article');
+        let articleIds = [];
+        $.each( articles, ( i, value ) => {
+            articleIds.push(value.id);
         });
+        return (articleIds);
+    }
+
+
+    const addDropOtions = (ids) => {
+        ids.forEach(id => {
+            console.log(`Adding drop Options to ${id}`)
+            $(`#${id}`).children(".post-container").prepend(`
+                <div class='drop-area-container'>
+                    <div class="drop-area">
+                        <button id="stream_${id}">Stream</button>
+                    </div>
+                </div>
+            `)
+            // $(`#drop_${id}`).bind('click', () => dropPic(id));
+        }); 
+    }
+
+    console.log('////////////////////////////////////////');
+    let initialIds = [];
+    const firstArticles = $('article');
+    $.each(firstArticles, (i, value) => {
+        initialIds.push(value.id);
     });
+    console.log(initialIds);
+    addDropOtions(initialIds);
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if(mutation.addedNodes.length){
+                mutation.addedNodes.forEach(x => {
+                    addDropOtions(getIdsForStreamContainer(x.id));
+                });
+            }
+        })
+    });
+    const endlessScrollSection = document.querySelector('#list-view-2');
+    observer.observe(endlessScrollSection, {
+        childList: true
+    })
     console.log('////////////////////////////////////////');
 }
 
-
+///////////////////////////////////////////////////////////////
 
 
 
